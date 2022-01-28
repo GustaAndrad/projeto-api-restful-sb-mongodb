a1202328c775a0ddfa0d1c8fdd004b7e73c49c25
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-28 11:52:10 -0300

    Projeto criado

---
+++ src/test/java/com/gusta/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusta.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
