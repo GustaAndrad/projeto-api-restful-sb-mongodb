1f47103483982f42ca3b6b50f91ff26644848335
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-30 12:03:38 -0300

    findById implementation

--- src/main/java/com/gusta/workshopmongo/services/UserService.java
+++ src/main/java/com/gusta/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gusta.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gusta.workshopmongo.domain.User;
 import com.gusta.workshopmongo.repository.UserRepository;
+import com.gusta.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
 	}
 
 }
