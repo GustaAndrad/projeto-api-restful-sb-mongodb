65d7fda5a7afafb0767072cdb5791f5334dc7a18
Gustavo Andrade <gusta.andradev@gmail.com>
2022-01-30 12:28:39 -0300

    Insercao de usuario com POST

--- src/main/java/com/gusta/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gusta/workshopmongo/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.gusta.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gusta.workshopmongo.domain.User;
 import com.gusta.workshopmongo.dto.UserDTO;
@@ -33,4 +37,14 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
--- src/main/java/com/gusta/workshopmongo/services/UserService.java
+++ src/main/java/com/gusta/workshopmongo/services/UserService.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.gusta.workshopmongo.domain.User;
+import com.gusta.workshopmongo.dto.UserDTO;
 import com.gusta.workshopmongo.repository.UserRepository;
 import com.gusta.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +25,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj); 
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
