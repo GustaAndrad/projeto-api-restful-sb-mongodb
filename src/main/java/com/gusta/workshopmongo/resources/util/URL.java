08ff31b71381afe9bc41c91c7955645e2534bc36
Gustavo Andrade <gusta.andradev@gmail.com>
2022-02-01 12:13:56 -0300

    Consulta com varios criterios

--- src/main/java/com/gusta/workshopmongo/resources/util/URL.java
+++ src/main/java/com/gusta/workshopmongo/resources/util/URL.java
@@ -2,6 +2,10 @@ package com.gusta.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -10,6 +14,16 @@ public class URL {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
 		}
 	}
 }
